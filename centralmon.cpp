715aa8465ae7126d8d17d3237eb4e1630f4422d2
Ben Kietzman <ben@kietzman.org>
2025-05-02 11:20:03 -0500

    Saved work.

--- centralmon.cpp
+++ centralmon.cpp
@@ -1,4 +1,3 @@
-// vim600: fdm=marker
 /* -*- c++ -*- */
 ///////////////////////////////////////////
 // Central Monitor:  centralmon
@@ -6,19 +5,9 @@
 // file       : centralmon.cpp
 // author     : Ben Kietzman
 // begin      : 2008-08-08
-// copyright  : kietzman.org
+// copyright  : Ben Kietzman
 // email      : ben@kietzman.org
 ///////////////////////////////////////////
-
-/**************************************************************************
-*                                                                         *
-*   This program is free software; you can redistribute it and/or modify  *
-*   it under the terms of the GNU General Public License as published by  *
-*   the Free Software Foundation; either version 2 of the License, or     *
-*   (at your option) any later version.                                   *
-*                                                                         *
-**************************************************************************/
-
 /*! \file centralmon.cpp
 * \brief Central Monitor Client Daemon
 *
--- centralmond.cpp
+++ centralmond.cpp
@@ -1,4 +1,3 @@
-// vim600: fdm=marker
 /* -*- c++ -*- */
 ///////////////////////////////////////////
 // Central Monitor:  centralmond
@@ -6,19 +5,9 @@
 // file       : centralmond.cpp
 // author     : Ben Kietzman
 // begin      : 2008-08-08
-// copyright  : kietzman.org
+// copyright  : Ben Kietzman
 // email      : ben@kietzman.org
 ///////////////////////////////////////////
-
-/**************************************************************************
-*                                                                         *
-*   This program is free software; you can redistribute it and/or modify  *
-*   it under the terms of the GNU General Public License as published by  *
-*   the Free Software Foundation; either version 2 of the License, or     *
-*   (at your option) any later version.                                   *
-*                                                                         *
-**************************************************************************/
-
 /*! \file centralmond.cpp
 * \brief Central Monitor Server Daemon
 *
